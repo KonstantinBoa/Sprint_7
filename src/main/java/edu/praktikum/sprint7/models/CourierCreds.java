ac0b1b1e3657ffb1adb053dfa0683ddfaca97cdb
Вовк Алексей Анатольевич <avovk@samokat.ru>
2025-05-07 20:38:00 +0300

    add data models

---
+++ src/main/java/edu/praktikum/sprint7/models/CourierCreds.java
@@ -0,0 +1,17 @@
+package edu.praktikum.sprint7.models;
+
+public class CourierCreds {
+
+    private String login;
+
+    private String password;
+
+    private CourierCreds(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public static CourierCreds credsFromCourier(Courier courier) {
+        return new CourierCreds(courier.getLogin(), courier.getPassword());
+    }
+}
